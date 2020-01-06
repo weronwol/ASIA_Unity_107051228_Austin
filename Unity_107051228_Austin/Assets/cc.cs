a49d1ab88c38988a327d87b6f03f63dd56c641f1
weronwol <55180739+weronwol@users.noreply.github.com>
2020-01-07 00:07:38 +0800

    202017

---
+++ Unity_107051228_Austin/Assets/cc.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cc : MonoBehaviour
+{
+    public Transform cam, target;
+
+    public float speed = 3;
+    private void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, target.position, 0.5f * Time.deltaTime * speed);
+        print(pos);
+        cam.position = pos;
+    }
+}
---
+++ Unity_107051228_Austin/Assets/dragon.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class dragon : MonoBehaviour
+
+{
+    private void Update()
+
+
+    {
+
+        Turn();
+        Run();
+       
+        Take();
+    }
+    public Rigidbody rigcatch;
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Bone" && (ani.GetCurrentAnimatorStateInfo(0).IsName("atk")))
+        {
+            Physics.IgnoreCollision(other, GetComponent<Collider>());
+            other.GetComponent<HingeJoint>().connectedBody = rigcatch;
+        }
+        if (other.name == "沙子" && (ani.GetCurrentAnimatorStateInfo(0).IsName("atk")))
+        {
+            Physics.IgnoreCollision(other, GetComponent<Collider>());
+            GameObject.Find("Bone").GetComponent<HingeJoint>().connectedBody = null;
+        }
+    }
+
+
+
+    #region 區域
+    [Header("dgspeed")]
+    [Range(1, 1000)]
+    public int speed = 20;
+    [Tooltip("dg's rotation speed")]
+    [Range(1f, 2000f)]
+    public float turn = 11.1f;
+    public bool mission;
+    public string name = "OZ";
+    #endregion
+    public Transform tran;
+    public Rigidbody rig;
+    public Animator ani;
+  
+
+
+
+    private void Run()
+    {
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("atk")) return;
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
+        ani.SetBool("Walk", v != 0);
+    }
+    private void Turn()
+    {
+        float f = Input.GetAxis("Horizontal");
+        tran.Rotate(0, turn * f * Time.deltaTime, 0);
+    }
+   
+    private void Take()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            ani.SetTrigger("atk");
+    }
+    private void Task()
+    { }
+}
